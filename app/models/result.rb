21bc4c12e437281f203259cea995bcf86428abb1
ovidiubite <ovidiu_bite10@hotmail.com>
2016-06-25 00:05:24 +0300

    final prediction algorithm

--- app/models/result.rb
+++ app/models/result.rb
@@ -66,8 +66,9 @@ class Result < ActiveRecord::Base
   end
 
   def self.prediction_algorithm(min_medie_admitere, max_medie_admitere, treshold)
-    a = 3
-    b = 10
+    # cresterea exponentiala
+    a = 1
+    b = 3
     x = (max_medie_admitere - min_medie_admitere)/2 + min_medie_admitere - treshold
     if( min_medie_admitere >= treshold)
        p = 1 / (1 + (1/a) * Math.exp((-1)*b*x).to_f)
@@ -88,7 +89,8 @@ class Result < ActiveRecord::Base
   end
 
   def self.predict_from_evaluation_results(evaluation_rate, graduation_rate, highschool_details)
-    prediction = -1
+    county = highschool_details.highschool.county
+    last_rate = highschool_details.last_rate
 
     this_year_results = EvaluationResult.select("position")
       .where("year = (?) AND county_id = (?)", Date.today.year, county.id)
@@ -99,68 +101,15 @@ class Result < ActiveRecord::Base
     pos1 = this_year_results.last.position
     pos2 = dr1.first.position
 
-    # @TODO continua pe evaluation results
-    # tabela de admitere din anul precedent
-    # trebuie inclus si anul si liceul...
-    if( (pos2 >= pos1) && (pos1 > 0) )
-      str2 = "select min(mediaadmitere), max(mediaadmitere) from
-             (select f1, position, mediaexamen, mediaadmitere, mediaabsolvire from
-             (select (row_number() over (order by mediaexamen desc)) as position, mediaexamen, mediaadmitere, mediaabsolvire, f1 from import2014 ) as imp2014
-             where position >= #{pos1} and position <= #{pos2}) as pos2014
-             where mediaabsolvire-0.1 <= #{graduation_rate} and mediaabsolvire+0.1 >= #{graduation_rate}"
+    str2 = "select min(admission_rate), max(admission_rate) from
+    (select position, evaluation_rate, admission_rate, graduation_rate from
+    (select position, evaluation_rate, admission_rate, graduation_rate from admission_results ) as presentYear
+    where county_id = #{county.id} AND year = #{Date.today.year} AND position >= #{pos1} AND position <= #{pos2}) as lastYear
+    where graduation_rate-0.1 <= #{graduation_rate} AND graduation_rate+0.1 >= #{graduation_rate}"
 
-      dr2 = ActiveRecord::Base.connection.execute(str2)
-      min_medie_admitere = dr2.values[0].to_f
-      max_medie_admitere = dr2.values[1].to_f
-      prediction_algorithm(min_medie_admitere, max_medie_admitere)
-    end
+    dr2 = ActiveRecord::Base.connection.execute(str2)
+    min_medie_admitere = dr2.values[0].to_f
+    max_medie_admitere = dr2.values[1].to_f
+    prediction_algorithm(min_medie_admitere, max_medie_admitere, last_rate)
   end
-
-  # algoritmul original
-  #
-  #
-  # def self.predict(evaluation_rate, graduation_rate, treshold)
-  #    prediction = -1
-  #
-  #    # tabela de evaluare nationala
-  #    # trebuie inclus si anul si liceul...
-  #    str1 = "select min(f1), max(f1) from import2015 where media-0.02 <= " + evaluation_rate.to_s + " and media+0.02 >= " + evaluation_rate.to_s + " ";
-  #    dr1 = ActiveRecord::Base.connection.execute(str1)
-  #
-  #    # integers
-  #    pos1 = dr1.values[0]
-  #    pos2 = dr1.values[1]
-  #
-  #    # tabela de admitere din anul precedent
-  #    # trebuie inclus si anul si liceul...
-  #    if( (pos2 >= pos1) && (pos1 > 0) )
-  #      str2 = "select min(mediaadmitere), max(mediaadmitere) from
-  #             (select f1, position, mediaexamen, mediaadmitere, mediaabsolvire from
-  #             (select (row_number() over (order by mediaexamen desc)) as position, mediaexamen, mediaadmitere, mediaabsolvire, f1 from import2014 ) as imp2014
-  #             where position >= #{pos1} and position <= #{pos2}) as pos2014
-  #             where mediaabsolvire-0.1 <= #{graduation_rate} and mediaabsolvire+0.1 >= #{graduation_rate}"
-  #
-  #      dr2 = ActiveRecord::Base.connection.execute(str2)
-  #      a = 3, b = 10
-  #      min_medie_admitere = dr2.values[0].to_f
-  #      max_medie_admitere = dr2.values[1].to_f
-  #
-  #      if( min_medie_admitere >= treshold)
-  #         x = (max_medie_admitere - min_medie_admitere)/2 + min_medie_admitere - treshold
-  #         p = 1 / (1 + (1/a) * Math.exp((-1)*b*x).to_f)
-  #      elsif( max_medie_admitere <= treshold )
-  #         x = (max_medie_admitere - min_medie_admitere)/2 + min_medie_admitere - treshold
-  #         p = 1 / (1 + a * Math.exp((-1)*b*x).to_f)
-  #      else
-  #         p = 0.25.to_f + ((treshold - min_medie_admitere) * 0.5.to_f) / (max_medie_admitere - min_medie_admitere)
-  #      end
-  #      prediction = p * 100
-  #      prediction.round
-  #    end
-  #
-  #    Result.create(evaluation_rate: evaluation_rate.to_s, graduation_rate: graduation_rate.to_s, percentage: prediction.to_s)
-  #
-  #    return prediction
-  #  end
-
 end
